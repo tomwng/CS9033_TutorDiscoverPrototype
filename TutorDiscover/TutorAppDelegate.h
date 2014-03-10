36f214e9ae30be8b15c65db61b0ea22a98830032
Tom <tomwng@gmail.com>
2014-03-10 11:03:26 -0400

    First commit

---
+++ TutorDiscover/TutorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TutorAppDelegate.h
+//  TutorDiscover
+//
+//  Created by Tom on 3/10/14.
+//  Copyright (c) 2014 Tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TutorDiscover/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TutorDiscover
+//
+//  Created by Tom on 3/10/14.
+//  Copyright (c) 2014 Tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TutorAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TutorAppDelegate class]));
+	}
+}
